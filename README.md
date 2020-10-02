1a6be1024f82ef5c15049feceb24e13434e9441a
yay17007 <yaqiongyao1993@gmail.com>
2020-10-02 19:01:53 -0400

    slides for ucsas 2020

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-UCSAS_WebScrapping
 
-Various sports websites produce tons of data every day. How to collect them might be the first difficulty that people encounter when they start doing sports analytics. We could collect these data by just copying and pasting manually, however, such way is tedious and time consuming. Web scrapping techniques provide an elegant way to solve this problem, which can capture information from website efficiently and reproducibly. In this workshop, topics include: 1) an overview of several web scrapping methods using R; 2) popular R packages for static and dynamic web pages, respectively. Examples are provided to illustrate how the web scrapping techniques are applied in the real world.
+[Slides](./ucsas.pdf) is for UCSAS 2020 workshop : Web Scraping for Sports Data with R.
+
+UCSAS_WebScraping
+
+Various sports websites produce tons of data every day. How to collect them might be the first difficulty that people encounter when they start doing sports analytics. We could collect these data by just copying and pasting manually, however, such way is tedious and time consuming. Web scrapping techniques provide an elegant way to solve this problem, which can capture information from website efficiently and reproducibly. In this workshop, topics include: 1) an overview of several web scrapping methods using R; 2) popular R packages for static and dynamic web pages, respectively. Examples are provided to illustrate how the web scraping techniques are applied in the real world.
+
+
+Acknowledgement
+
+The slides are modified from [Dr. Kovalchik's material](https://github.com/skoval/UseRSportTutorial) and [Wanwan Xu's slides](https://github.com/wanwanx/WebScraping_UCSAS).
+
+
+
+
--- code.R
+++ code.R
@@ -1,11 +1,53 @@
 
 ## Web scraping using RSelenium
 require("RSelenium")
-rD <- rsDriver(port = 5559L, chromever = "85.0.4183.87")
+rD <- rsDriver(port = 5533L, chromever = "85.0.4183.87")
 remDr <- rD$client
-remDr$navigate("http://www.google.com")
-url <- "http://www.flashscore.com/match/Cj6I5iL9/#match-statistics;0"
+url <- "https://www.flashscore.com/team/connecticut-huskies/8rqVf3Tj/results/#"
 remDr$navigate(url)
-webElem <- remDr$findElements(using = 'id', "detail")
-unlist(lapply(webElem, function(x){x$getElementText()}))[[1]]
+
+repeat{
+  x <- try(webElemMore <- 
+             remDr$findElement(using = 'xpath', 
+                               '//*[@id="live-table"]/div[1]/div/div/a'),
+           silent=T)
+  if (inherits(x, "try-error")) break
+  webElemMore$clickElement()
+}
+
+webElemTime <- remDr$findElements(using = 'xpath', 
+                              '//*[@class="event__time"]')
+webElemTime <- 
+  unlist(lapply(webElemTime, function(x){x$getElementText()}))
+webElemTime <- gsub("\\n", " ", webElemTime)
+
+webElemHome <- 
+  remDr$findElements(using = 'class', 
+                     'event__participant')
+webElemHome <- 
+  unlist(lapply(webElemHome, function(x){x$getElementText()}))
+
+webElemScore <- 
+  remDr$findElements(using = 'class', 'event__score')
+webElemScore <- 
+  unlist(lapply(webElemScore, function(x){x$getElementText()}))
+
+webElemResult <- 
+  remDr$findElements(using = 'class', 'wld')
+webElemResult <- 
+  unlist(lapply(webElemResult, function(x){x$getElementText()}))
+
+n <- length(webElemHome)
+basketball <- 
+  data.frame(time = webElemTime,
+             Home = webElemHome[seq(n) %% 2 == 1],
+             Away = webElemHome[seq(n) %% 2 == 0],
+             HomeS = webElemScore[seq(n) %% 2 == 1],
+             AwayS = webElemScore[seq(n) %% 2 == 0],
+             Result = webElemResult)
+
+head(basketball)
 remDr$close()
+
+
+
