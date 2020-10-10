e4b8da34c18ee5934bf5ff91c0e6c5134d3e1b9b
yay17007 <yaqiongyao1993@gmail.com>
2020-10-10 12:45:37 -0400

    typos

--- code.R
+++ code.R
@@ -1,19 +1,56 @@
 
+## import data from websites directly
+
+url <- "http://www.tennis-data.co.uk/2020/ausopen.csv"
+tennis_aus <- read.csv(url)
+str(tennis_aus)
+
+## webscraping using rvest
+
+require(rvest)
+url_elo <- "http://tennisabstract.com/reports/atp_elo_ratings.html"
+webpage <- read_html(url_elo)
+elo_class <- webpage %>% 
+  html_nodes(".tablesorter") %>% 
+  html_table()
+elo_id <- webpage %>% 
+  html_nodes("#reportable") %>% 
+  html_table()
+identical(elo_class, elo_id)
+
+
 ## Web scraping using RSelenium
 require("RSelenium")
-rD <- rsDriver(port = 5533L, chromever = "85.0.4183.87")
+rD <- rsDriver(port = 5563L, chromever = "85.0.4183.87")
 remDr <- rD$client
-url <- "https://www.flashscore.com/team/connecticut-huskies/8rqVf3Tj/results/#"
+url <- "http://www.flashscore.com/match/Cj6I5iL9/#match-statistics;0"
+remDr$navigate(url)
+webElem <- remDr$findElements(using = 'class', "statBox")
+webElem <- unlist(lapply(webElem, function(x){x$getElementText()}))[[1]]
+head(unlist(strsplit(webElem, split = '\n')))
+remDr$close()
+
+
+
+
+
+rD <- rsDriver(port = 5572L, chromever = "85.0.4183.87")
+remDr <- rD$client
+url <- "https://www.flashscore.com/team/connecticut-huskies/8rqVf3Tj/results/"
 remDr$navigate(url)
 
+
 repeat{
-  x <- try(webElemMore <- 
-             remDr$findElement(using = 'xpath', 
-                               '//*[@id="live-table"]/div[1]/div/div/a'),
-           silent=T)
-  if (inherits(x, "try-error")) break
-  webElemMore$clickElement()
+  b <- tryCatch({
+    suppressMessages({
+      webElemMore <- remDr$findElement(using = 'xpath', 
+                        '//*[@id="live-table"]/div[1]/div/div/a')
+      webElemMore$clickElement()
+    })
+  }, error = function(e) e)
+  if(inherits(b, "error")) break
 }
+
 
 webElemTime <- remDr$findElements(using = 'xpath', 
                               '//*[@class="event__time"]')
